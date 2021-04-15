4de0d16c7aab40ccdab9a5266f53a66ddf40583f
jaccyli <stevobs@163.com>
2021-04-16 00:13:39 +0800

    update

--- array/array.go
+++ array/array.go
@@ -1,5 +1,7 @@
 package array
 
+import "errors"
+
 type Array interface {
 	Clear()
 	String()
@@ -26,31 +28,41 @@ func (a *ArrayList) GetSize() int64 {
 	return a.ArraySize
 }
 
-// [1, 2, 3, 4]
-func (a *ArrayList) InsertElement(index int64, data interface{}) {
-	if a.ArraySize >= int64(cap(a.DataStore)) {
-		new := new(ArrayList)
-		new.DataStore = make([]interface{}, 0, a.ArraySize*2)
-		for i := a.ArraySize; i >= index; i-- {
-			a.DataStore[i+1] = a.DataStore[i]
-			if i == index {
-				a.DataStore[index] = data
-			}
-		}
-		//tmp := a.DataStore[index+1]
-		//new.DataStore = append(append(a.DataStore[:index], data), (a.DataStore[index+1:])...)
-		//a.DataStore = new.DataStore
-		//a.DataStore[index+1] = tmp
-		a.ArraySize++
-		return
-	}
-	a.DataStore = append(append(a.DataStore[:index], data), (a.DataStore[index+1:])...)
-	a.ArraySize++
-}
-
 func (a *ArrayList) AppendElement(data interface{}) {
 	a.DataStore = append(a.DataStore, data)
 	a.ArraySize++
+}
+
+func (a *ArrayList) DeleteElement(index int64) error {
+	if index < 0 || index > a.ArraySize {
+		return errors.New("index out of range")
+	}
+	a.DataStore = append(a.DataStore[:index], a.DataStore[index+1:])
+	a.ArraySize--
+	return nil
+}
+
+func (a *ArrayList) InsertElement(index int64, data interface{}) error {
+	if index < 0 || index > a.ArraySize {
+		return errors.New("index out of range")
+	}
+	// if array is full, double the ram
+	a.isFull()
+	a.DataStore = a.DataStore[:a.ArraySize+1]
+	for i := a.ArraySize; i > index; i-- {
+		a.DataStore[i] = a.DataStore[i-1]
+	}
+	a.DataStore[index] = data
+	a.ArraySize++
+	return nil
+}
+
+func (a *ArrayList) SetElement(index int64, data interface{}) error {
+	if index < 0 || index >= a.ArraySize {
+		return errors.New("index out of range")
+	}
+	a.DataStore[index] = data
+	return nil
 }
 
 func (a *ArrayList) String() {
@@ -58,5 +70,14 @@ func (a *ArrayList) String() {
 }
 
 func (a *ArrayList) Clear() {
+	a.DataStore = make([]interface{}, 0, 20)
+	a.ArraySize = 0
+}
 
+func (a *ArrayList) isFull() {
+	if a.ArraySize == int64(cap(a.DataStore)) {
+		new := make([]interface{}, 2*a.ArraySize)
+		copy(new, a.DataStore)
+		a.DataStore = new
+	}
 }
--- main.go
+++ main.go
@@ -8,30 +8,18 @@ import (
 
 func main() {
 	a := array.NewArrayList()
-	a.AppendElement(20)
-	a.AppendElement(250)
-	a.AppendElement(340)
-	a.AppendElement(309)
-	a.AppendElement(600)
+	a.AppendElement(0)
+	for i := 1; i < 10; i++ {
+		err := a.InsertElement(a.ArraySize, i)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 	fmt.Println(a.DataStore)
-	a.InsertElement(3, 22)
-	s := a.GetSize()
-	fmt.Println(s)
-	fmt.Println(a.DataStore)
-
-	var ia = []int{1, 2, 3, 4, 5, 6, 7, 8}
-	x := 0
-	data := 999
-	b := ia[:x]
-	c := ia[x]
-	d := ia[x+1:] // x+1
-	tmp := ia[x+1]
-	fmt.Println("db: ", d)
-	new := append(b, c, data)
-	fmt.Println("da: ", d)
-	fmt.Println("new: ", new)
-	new = append(new, d...)
-	new[x+2] = tmp
-
-	fmt.Printf("%#v, %#v, %#v, %#v\n", b, c, d, new)
+	for i := 0; i < 3; i++ {
+		if err := a.DeleteElement(3); err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(a.DataStore)
+	}
 }
